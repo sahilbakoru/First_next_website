1aafa2fdee68b1d0d795dec0982202bb2d693051
sahilbakoru <sahilbakoru1999@gmail.com>
2023-05-16 12:20:13 +0530

    some section added

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,23 @@
-
-
+import  './globals.css'
+import { DM_Serif_Display} from 'next/font/google';
+const inter = DM_Serif_Display({ weight: '400',subsets: ['latin'] });
 export default function Home() {
+
   return (
-    <div>Welcome Sahil</div>
+  <main className={inter.className}>
+
+ <div style={{textAlign:'center',borderBottom:'1px solid lightgrey',paddingTop:'5%',paddingBottom:'10%'}}>
+  <div style={{fontSize:'400%'}}>Sahil Bakoru ,</div>
+ </div> 
+ <div style={{textAlign:'center',borderBottom:'1px solid lightgrey',paddingTop:'5%',paddingBottom:'10%'}}>
+  <div style={{fontSize:'400%'}}>Projects.</div>
+ </div> 
+ <div style={{textAlign:'center',borderBottom:'1px solid lightgrey',paddingTop:'5%',paddingBottom:'10%'}}>
+  <div style={{fontSize:'400%'}}>Booklist.</div>
+ </div> 
+ 
+
+  </main>
+
   )
 }
