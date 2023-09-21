e67f9914fc8044a2414ec5687344d90c4fdf78a5
sahilbakoru <sahilbakoru1999@gmail.com>
2023-09-22 00:34:56 +0530

    test image added

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,6 +1,31 @@
-
-export default function About() {
+import { DM_Serif_Display } from "next/font/google";
+const inter = DM_Serif_Display({ weight: "400", subsets: ["latin"] });
+import Image from "next/image";
+import '/public/mypos.png'
+export default function Projects() {
   return (
-  <div>Projects</div>
-  )
+    <main style={{marginLeft:'10%',marginRight:'10%'}} className={inter.className}>
+      <div
+        style={{
+          textAlign: "center",
+          paddingTop: "5%",
+          paddingBottom: "4%",
+        }}
+      >
+        <div style={{ fontSize: "400%" }}>Projects</div>
+      </div>
+
+      <div
+        style={{
+          border: "0.5px solid grey",
+          marginLeft: "10%",
+          marginRight: "10%",
+        }}
+      />
+           <Image
+           style={{alignSelf:'center'}}
+       width={500}
+        height={500} src='/mypos.png'  alt="img" />
+    </main>
+  );
 }
